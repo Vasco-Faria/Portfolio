0dcaaf223905e21db9837cebc7bfd0069bd36c72
Vasco_Faria <vascomfaria@ua.pt>
2024-02-13 00:51:11 +0000

    Removing Quote

--- portfolioweb/src/App.js
+++ portfolioweb/src/App.js
@@ -5,7 +5,6 @@ import Navbar from './Sections/Navbar/Navbar';
 import Home from './Sections/Home/Home';
 import AboutMe from './Sections/AboutMe/AboutMe';
 import Timeline from './Sections/Timeline/Timeline';
-import PopUp from './Components/PopUp/PopUp';
 import Projects from './Sections/Projects/Projects';
 import ContactMe from './Sections/ContactMe/ContactMe';
 import BackToTop from './Components/BackToTop/BackToTop';
@@ -16,7 +15,6 @@ function App() {
   return (
     <div className="App">
       <BackToTop/>
-      <PopUp/>
       <Navbar/>
       <Home/>
       <AboutMe/>  
