f9b17ad06898e36ab6af9f7c2b97ab1dd4656307
Vasco_Faria <vascomfaria@ua.pt>
2024-06-08 13:54:52 +0100

    change snow delete

--- portfolioweb/src/Sections/Home/Home.js
+++ portfolioweb/src/Sections/Home/Home.js
@@ -1,34 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import Snowflake from '../../Assets/SnowFlakes/SnowFlakes';
+import React from 'react';
 import HomeTitle from '../../Components/HomeTitle/HomeTitle';
 import './Home.css';
 
 const Home = () => {
-    const [snowflakes, setSnowflakes] = useState([]);
-  
-    useEffect(() => {
-      const generateSnowflake = () => {
-        const left = `${Math.random() * 100}vw`;
-        const animationDuration = `${2 + Math.random() * 3}s`;
-  
-        return { id: Date.now(), component: <Snowflake key={Date.now()} left={left} animationDuration={animationDuration} /> };
-      };
-  
-      const snowflakeInterval = setInterval(() => {
-        setSnowflakes((prevSnowflakes) => {
-          const newSnowflake = generateSnowflake();
-          const updatedSnowflakes = [newSnowflake, ...prevSnowflakes.slice(0, 19)]; 
-          return updatedSnowflakes;
-        });
-      }, 500);
-  
-      return () => clearInterval(snowflakeInterval);
-    }, []);
+    
   
     return (
       <div id="snow-container"className="home-section">
         <HomeTitle />
-        <div className="snowflakes">{snowflakes.map((snowflake) => snowflake.component)}</div>
       </div>
     );
   };
